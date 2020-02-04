d05b07a99feef156732f8723b5aeb2aa476b1009
JoeyPirich <joeypirich@hotmail.com>
2020-02-03 19:12:47 -0800

    Start on shooter
    began pid

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -1,13 +1,13 @@
 package frc.robot;
 
 public class Ports{
-    //We don't know if these ports are true yet.
-    public static final int LEFT_DRIVETRAIN_1 = 2;
-    public static final int LEFT_DRIVETRAIN_2 = 3;
-    public static final int RIGHT_DRIVETAIN_1 = 5;
-    public static final int RIGHT_DRIVETAIN_2 = 4;
-    public static final int GYRO_PORT = 6;
-    
+     
+    public static final int LEFT_DRIVETRAIN_1 = 1;
+    public static final int LEFT_DRIVETRAIN_2 = 4;
+    public static final int RIGHT_DRIVETAIN_1 = 2;
+    public static final int RIGHT_DRIVETAIN_2 = 3;
+    public static final int GYRO_PORT = 5;
+    public static final int SHOOTER_PORT = 0;
 }
 
 
