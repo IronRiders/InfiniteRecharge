83b9501e2177b9a52c8083cf1c27cd4c97f19e2c
Jacob6819 <45805625+Jacob6819@users.noreply.github.com>
2020-02-03 20:54:44 -0800

    Changed shoot method

--- src/main/java/frc/robot/Shooter.java
+++ src/main/java/frc/robot/Shooter.java
@@ -5,6 +5,7 @@ import edu.wpi.first.wpilibj.Encoder;
 import com.revrobotics.CANEncoder;
 import com.revrobotics.CANPIDController;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.ControlType;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class Shooter {
@@ -13,10 +14,12 @@ public class Shooter {
     public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput, maxRPM;
     public int countsPerRevolution;
     public CANSparkMax shooterMotor;
+    public double wheelRadius = 3;
+
     public Shooter(int portNum) {
         shooterMotor = new CANSparkMax(portNum, MotorType.kBrushless);
         shootEncoder = shooterMotor.getEncoder();
-        shootEncoder.getCountsPerRevolution();
+        countsPerRevolution = shootEncoder.getCountsPerRevolution();
         
         shooterMotor_PIDcontroller = shooterMotor.getPIDController();
         shooterMotor_PIDcontroller.setP(kP);
@@ -27,10 +30,13 @@ public class Shooter {
         shooterMotor_PIDcontroller.setOutputRange(kMinOutput, kMaxOutput);
         
         
+        
     }
 
 
     public  void shoot(double velocity) {
+        velocity = (velocity *countsPerRevolution)/ (2 *Math.PI *wheelRadius);
         shooterMotor.set(velocity);
+        shooterMotor_PIDcontroller.setReference(velocity, ControlType.kVelocity);
     }
 }
