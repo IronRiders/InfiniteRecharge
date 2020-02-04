399da7a5e8df0d9b1475828d88be4ee0a73cfb67
Jacob6819 <45805625+Jacob6819@users.noreply.github.com>
2020-02-03 19:56:11 -0800

    Started to add an Encoder
    We should look into if the spark max built in encoder has a function for velocity.

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -8,6 +8,9 @@ public class Ports{
     public static final int RIGHT_DRIVETAIN_2 = 3;
     public static final int GYRO_PORT = 5;
     public static final int SHOOTER_PORT = 0;
+
+
+    public static double velocity;
 }
 
 
--- src/main/java/frc/robot/Shooter.java
+++ src/main/java/frc/robot/Shooter.java
@@ -1,18 +1,23 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Encoder;
+
+import com.revrobotics.CANEncoder;
 import com.revrobotics.CANPIDController;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class Shooter {
-
+    public CANEncoder shootEncoder;
     private CANPIDController shooterMotor_PIDcontroller;
     public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput, maxRPM;
-
+    public int countsPerRevolution;
     public CANSparkMax shooterMotor;
     public Shooter(int portNum) {
         shooterMotor = new CANSparkMax(portNum, MotorType.kBrushless);
-
+        shootEncoder = shooterMotor.getEncoder();
+        shootEncoder.getCountsPerRevolution();
+        
         shooterMotor_PIDcontroller = shooterMotor.getPIDController();
         shooterMotor_PIDcontroller.setP(kP);
         shooterMotor_PIDcontroller.setI(kI);
@@ -20,7 +25,8 @@ public class Shooter {
         shooterMotor_PIDcontroller.setIZone(kIz);
         shooterMotor_PIDcontroller.setFF(kFF);
         shooterMotor_PIDcontroller.setOutputRange(kMinOutput, kMaxOutput);
-
+        
+        
     }
 
 
