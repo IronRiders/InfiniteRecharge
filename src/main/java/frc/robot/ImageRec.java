662c0e73a2b11851248eeb46e56ae526f4b09ad7
software@ironriders.org <ishan.f.coutinho@gmail.com>
2020-02-05 15:27:35 -0800

    Added guesstimation of distances and power output
    Signed-off-by: software@ironriders.org <ishan.f.coutinho@gmail.com>

--- src/main/java/frc/robot/ImageRec.java
+++ src/main/java/frc/robot/ImageRec.java
@@ -5,6 +5,12 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.NetworkTable;
 
 public class ImageRec {
+    private final double targetX = 0;
+    private final double targetHeight = 100;
+    private final double[] distanceTuning = new double[]{10, 0.1};
+    private final double[] alignmentTuning = new double[]{0, 100};
+    private final double[] powerTuning = new double[]{10, 1000};
+
     private NetworkTableEntry areaNTE;
     private NetworkTableEntry centerXNTE;
     private NetworkTableEntry centerYNTE;
@@ -27,7 +33,7 @@ public class ImageRec {
         heightNTE = table.getEntry("height");
     }
     
-    public void update() {
+    private void update() {
         areaNTE.setDouble(area);
         centerXNTE.setDouble(centerX);
         centerYNTE.setDouble(centerY);
@@ -35,5 +41,21 @@ public class ImageRec {
         heightNTE.setDouble(height);
     }
 
-    // TODO: everything else
+    // First element is horizontal angle offset, second element is release velocity
+    public double[] aim() {
+        update();
+
+        // Convert height difference to distance away from target
+        // When testing put distance on the dashboard
+        // If a linear term can't reliably compute the distance, add higher power terms
+        double distanceFromTarget = (targetHeight - height) * distanceTuning[1] + distanceTuning[0];
+
+        // Do the same for distance that we're off horizontally
+        double horizontalAlignment = (targetX - centerX) * alignmentTuning[1] + alignmentTuning[0];
+
+        // Use the same procedure for converting distance to power
+        double shotPower = distanceFromTarget * powerTuning[1] + powerTuning[0];
+
+        return new double[]{horizontalAlignment, shotPower};
+    }
 }
