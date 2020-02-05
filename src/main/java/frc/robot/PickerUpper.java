9bd0ae7fe6abf4644cbf7665c4b9fcf1dbb1c34b
Jacob6819 <45805625+Jacob6819@users.noreply.github.com>
2020-02-04 16:48:00 -0800

    Added a Picker Upper

---
+++ src/main/java/frc/robot/PickerUpper.java
@@ -0,0 +1,24 @@
+package frc.robot;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+public class PickerUpper{
+    private final CANSparkMax pickerUpperMotor;
+
+    public PickerUpper (int port)  {
+        pickerUpperMotor = new CANSparkMax(port, MotorType.kBrushless);
+
+    }
+
+    public void pickUp() {
+        pickerUpperMotor.set(.5);
+
+
+    }
+    
+    public void stopPickingUp() {
+        pickerUpperMotor.set(0);
+
+    }
+}
