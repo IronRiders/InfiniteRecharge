824baad6e962e04ad9a1132950826b420e632b12
Jacob6819 <45805625+Jacob6819@users.noreply.github.com>
2020-02-05 16:48:31 -0800

    Put climber into brake mode

--- src/main/java/frc/robot/Climber.java
+++ src/main/java/frc/robot/Climber.java
@@ -1,6 +1,7 @@
 package frc.robot;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class Climber {
@@ -10,8 +11,9 @@ public class Climber {
     public Climber(int Port1, int Port2){
         pullUpMotor = new CANSparkMax(Port1, MotorType.kBrushed);
         pullDownMotor = new CANSparkMax(Port2, MotorType.kBrushed);
-
-
+        pullDownMotor.setIdleMode(IdleMode.kBrake);
+        pullUpMotor.setIdleMode(IdleMode.kBrake);
+    
     }
 
     public void pullUp(){
