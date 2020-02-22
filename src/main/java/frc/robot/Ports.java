bd86b21134eb84ec32d454effd6e06e3ba8d3fdf
Ishanthecoder <ishan.f.coutinho@gmail.com>
2020-02-22 15:49:35 -0800

    added correct ports

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -7,15 +7,15 @@ public class Ports{
     public static final int RIGHT_DRIVETAIN_1 = 2;
     public static final int RIGHT_DRIVETAIN_2 = 3;
     public static final int GYRO_PORT = 5;
-    public static final int SHOOTER_PORT = 0;
-    public static final int INDEX_MOTOR_1 = 0;
-    public static final int INDEX_MOTOR_2 = 0;
+    public static final int SHOOTER_PORT = 5;
+    public static final int INDEX_MOTOR_1 = 10;
+    public static final int INDEX_MOTOR_2 = 11;
     public static final int LINE_BREAKER_PORT = 0;
 
     public static final int PULL_UP = 0;
     public static final int PULL_DOWN = 0;
-    public static final int DRAWBRIDGE = 0;
-    public static final int PICKERUPPER = 0;
+    public static final int DRAWBRIDGE = 8;
+    public static final int PICKERUPPER = 9;
 
     public static double velocity;
 
